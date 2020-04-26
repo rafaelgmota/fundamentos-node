a3f1ec03d65fb05e83ebeb0eddb93c6d1de49fbe
Rafael Mota <gmotarafael@gmail.com>
2020-04-26 18:19:38 -0300

    Add transactions routes

---
+++ src/services/GetTransactionsService.ts
@@ -0,0 +1,30 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface Response {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+class GetTransationsService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute(): Response {
+    const transactions = this.transactionsRepository.all();
+    const balance = this.transactionsRepository.getBalance();
+
+    return { transactions, balance };
+  }
+}
+
+export default GetTransationsService;
